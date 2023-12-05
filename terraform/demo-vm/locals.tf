d7ef7b2dccc890e03f008f87a218252038e383c4
dmaxim <danemaxim@gmail.com>
2023-12-05 18:02:24 -0500

    Add shebang to custom data

--- terraform/demo-vm/locals.tf
+++ terraform/demo-vm/locals.tf
@@ -5,6 +5,7 @@ locals {
     environment = var.environment
   }
   vm_script = <<CUSTOM_DATA
+  #!/usr/bin/env bash
   crontab -l > initcron
   echo "0 0 * * * wget https://en.wikipedia.org/wiki/Philadelphia_chromosome -O /var/tmp/wiki-download.htm" >> initcron
   rm initcron
